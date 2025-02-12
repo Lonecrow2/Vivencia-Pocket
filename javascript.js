96c06ecb385da2f0a8ede4c63a5d6fd22be073b5
Ernesto Rampanelli Neto <ghostnetto@gmail.com>
2025-02-12 17:26:16 -0300

    Rearumei a funcção copyright

--- javascript.js
+++ javascript.js
@@ -1,7 +1,8 @@
 function copyright() {
     var list = document.getElementById("copyright-list");
-    if (list.style.display === "none") {
+    if (list.style.display === "none" || list.style.display === "") {
         list.style.display = "block";
+    
     } else {
         list.style.display = "none";
     }
